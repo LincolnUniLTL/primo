ba1a25e08c6b496b768006a831b31f7a581fa84b
deborahfitchett <deborah.fitchett@lincoln.ac.nz>
2015-07-27 15:15:15 +1200

    Automated warning note display on Primo when Alma or Primo malfunction

---
+++ warning_note.js.php
@@ -0,0 +1,7 @@
+﻿/* This is a placeholder - please refer to https://github.com/LincolnUniLTL/LTLstats
+   The files there:
+     * /connections/exlibris_status.php
+	 * cache.php
+	 * warning_note.js.php
+	 * with reference to the README.md there
+   can be used to provide an automated warning note (or manual override) at the top of your Primo pages, based on the status of your Alma/Primo hosted instances as described on the Ex Libris Status website.  */
